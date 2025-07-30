4979e4d51d94e941381f23a8fe17e335bb63a869
CormacC30 <cormaccostello2@gmail.com>
2025-07-30 15:56:02 +0100

    created logging script

--- ping-logging.py
+++ ping-logging.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
 def ping():
+    app.logger.info('Ping Pong')
     return jsonify({"message": "pong"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
