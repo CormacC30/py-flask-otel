4bc4b3fb58ffa551213a3af1d234f77180675d57
CormacC30 <cormaccostello2@gmail.com>
2025-08-01 08:51:55 +0100

    logging configured properly

--- ping-logging.py
+++ ping-logging.py
@@ -1,7 +1,11 @@
 import flask
 from flask import Flask, jsonify
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+app.logger.setLevel(logging.INFO)
 
 @app.route('/ping', methods=['GET'])
 def ping():
@@ -9,5 +13,6 @@ def ping():
     return jsonify({"message": "pong"}), 200
 
 if __name__ == '__main__':
+    app.logger.info('Server is starting....')
     app.run(debug=True, host='0.0.0.0', port=5000)
 
